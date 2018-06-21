593ef2d276b10578e972fda9b850da21ba91c986
David Sancho <dsancho@dsancho.local>
2018-06-21 18:22:42 +0200

    added functions dir & file

--- common-functions/pipeline-utils.groovy
+++ common-functions/pipeline-utils.groovy
@@ -1,14 +1,12 @@
 // pipeline-utils functions
 
-def createConfigMap(name, project, pathToFiles) {
-   echo "Creating configmap ${name} in project ${project} using files under path: ${pathToFiles}"
-   openshift.withProject( project ) {
-	// create temporal configmap and replace current one
-	def configMap = openshift.create( "${name}-tmp", "configmap", "--from-file", pathToFiles ).object(exportable:true)
-	configMap.metadata.name = name
-	openshift.apply(configMap)  	
-	openshift.selector( "configmap/${name}-tmp" ).delete()	
-   }
+def createConfigMap(name, pathToFiles) {
+   echo "Creating configmap ${name} using files under path: ${pathToFiles}"
+   // create temporal configmap and replace current one
+   def configMap = openshift.create( "${name}-tmp", "configmap", "--from-file", pathToFiles ).object(exportable:true)
+   configMap.metadata.name = name
+   openshift.apply(configMap)  	
+   openshift.selector( "configmap/${name}-tmp" ).delete()	
 }
 
 return this
