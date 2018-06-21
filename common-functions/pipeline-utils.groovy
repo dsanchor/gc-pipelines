508c9d1cf458a5c5c6f7e16c999c9c7eead87bb1
David Sancho <dsancho@dsancho.local>
2018-06-21 18:48:23 +0200

    fixed naming

--- common-functions/pipeline-utils.groovy
+++ common-functions/pipeline-utils.groovy
@@ -3,7 +3,7 @@
 def createConfigMap(name, pathToFiles) {
    echo "Creating configmap ${name} using files under path: ${pathToFiles}"
    // create temporal configmap and replace current one
-   def configMap = openshift.create( "${name}-tmp", "configmap", "--from-file", pathToFiles ).object(exportable:true)
+   def configMap = openshift.create( "configmap", "${name}-tmp", "--from-file", pathToFiles ).object(exportable:true)
    configMap.metadata.name = name
    openshift.apply(configMap)  	
    openshift.selector( "configmap/${name}-tmp" ).delete()	
